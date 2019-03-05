d45db49c4b5a259dfe32ce89fd4b6b4c8e7af975
Romain Beaussart <romain.beaussart@efrei.net>
2019-03-05 18:12:32 +0100

    Data Bases

--- theatre.sql
+++ theatre.sql
@@ -1,20 +1,23 @@
-/*---------------
- |   SETTINGS   |
- ---------------*/
+/*==================================================================================
+                            |   SETTINGS   |
+ ==================================================================================*/
+
 
  ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY' ;
 
-/*---------------
- |  DATA BASES  | 
- ---------------*/
+
+/*==================================================================================
+                            |  DATA BASES  | 
+ ==================================================================================*/
+
 
 /* --- Fake date --- */
 
 CREATE TABLE FAKE_DATE
 (
-    DAY 	NUMBER(2) NOT NULL,
-    MONTH 	NUMBER(2) NOT NULL,
-    YEAR 	NUMBER(4) NOT NULL
+    DAY     NUMBER(2) NOT NULL,
+    MONTH   NUMBER(2) NOT NULL,
+    YEAR    NUMBER(4) NOT NULL
 );
 
 
@@ -22,16 +25,16 @@ CREATE TABLE FAKE_DATE
 
 CREATE TABLE THEATERS
 (
-    NAME		VARCHAR2(100) NOT NULL UNIQUE,
-    CAPACITY		NUMBER(6),		
+    NAME		        VARCHAR2(100) NOT NULL UNIQUE,
+    CAPACITY		    NUMBER(6),		
     BUDGET              NUMBER(6),
     CITY                VARCHAR2(100),
     
         /* Constraints */
 
-    CONSTRAINT PK_THEATER           PRIMARY KEY(THEATER_ID),
-    CONSTRAINT NN_THEATER_CAP       CHECK(CAPACITY IS NOT NULL),
-    CONSTRAINT NN_THEATER_BUD       CHECK(BUDGET > 0)
+    CONSTRAINT PK_THEATERS          PRIMARY KEY(THEATER_ID),
+    CONSTRAINT NN_THEATERS_CAP      CHECK(CAPACITY IS NOT NULL),
+    CONSTRAINT NN_THEATERS_BUD      CHECK(BUDGET > 0)
 );
 
 
@@ -40,42 +43,73 @@ CREATE TABLE THEATERS
 CREATE TABLE SHOWS
 (
     ID                  NUMBER(6),
-    NAME       		VARCHAR(100) NOT NULL UNIQUE,
-    COST		NUMBER(6),
-    THEATER_PROD	VARCHAR(100),
+    NAME       			VARCHAR(100) NOT NULL UNIQUE,
+	COST				NUMBER(6),
+	THEATER_PROD		VARCHAR(100),
     
         /* Constraints */
 
-    CONSTRAINT PK_SHOWS         PRIMARY KEY (ID),
-    FOREIGN KEY (NAME)          REFERENCES THEATERS (NAME),
-    CONSTRAINT NN_COUT          CHECK(COST IS NOT NULL)
+    CONSTRAINT PK_SHOWS             PRIMARY KEY (ID),
+    FOREIGN KEY (THEATER_PROD)      REFERENCES THEATERS(NAME),
+    CONSTRAINT NN_SHOWS_COST        CHECK(COST IS NOT NULL)
 );
+
 
 /* --- Representations --- */
 
 CREATE TABLE REPRESENTATIONS
 (
-    ID				NUMBER(6),
-    SHOW			VARCHAR2(100),
-    THEATER			VARCHAR2(100),
-    REPRESENTATION_DATE		DATE,
-    NUMB_OF_REPRESENTATION	NUMBER(5),
-    ACTOR_FEES			NUMBER(5),
-    STAGING_COST		NUMBER(5),
-    LIGHTING_COST		NUMBER(5),
-    TRAVEL_COST			NUMBER(5),
-    DISPONIBILITY		NUMBER(5),
+    ID       		                NUMBER(6),
+    SHOW_ID      				    NUMBER(6),
+	THEATER					        VARCHAR2(100),
+    REPRESENTATION_DATE           	DATE,
+	NUMB_OF_REPRESENTATION			NUMBER(5),
+	ACTOR_FEES						NUMBER(5),
+	STAGING_COST					NUMBER(5),
+	LIGHTING_COST					NUMBER(5),
+	TRAVEL_COST						NUMBER(5),
+    DISPONIBILITY                   NUMBER(5),
 
         /* Constraints */
 
     CONSTRAINT PK_REPRESENTATIONS           PRIMARY KEY(ID),
-    FOREIGN KEY (SHOW)                      REFERENCES SHOWS (NAME),
-    FOREIGN KEY (THEATER)                   REFERENCES THEATERS (NAME),
+    FOREIGN KEY (SHOW_ID)                   REFERENCES SHOWS(ID),
+    FOREIGN KEY (THEATER)                   REFERENCES THEATERS(NAME),
 );
 
-/*---------------
- |     DATA     | 
- ---------------*/
+
+/* --- Tickets --- */
+
+CREATE TABLE TICKETS
+(	
+	ID					    NUMBER(6),
+    T_TYPE        			NUMBER(5),
+    TARIF			  	    NUMBER(5),
+    REPRESENTATION_ID		NUMBER(6),
+    PURCHASING_DATE         DATE,
+
+        /* Constraintes */
+
+    CONSTRAINT PK_TICKETING         	PRIMARY KEY(ID),
+    FOREIGN KEY(REPRESENTATION_ID)      REFERENCES REPRESENTATIONS(ID),
+    CONSTRAINT NN_TICKETING_TARIF   	CHECK(TARIF IS NOT NULL)
+
+    
+    
+);
+
+/*==================================================================================
+                            |   TRIGGERS   | 
+ ==================================================================================*/
+
+
+/* --- Theaters --- */
+
+
+/*==================================================================================
+                            |     DATA     | 
+ ==================================================================================*/
+
 
 /* --- Theaters --- */
 
